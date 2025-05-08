6a74bbe57fb18d4b265d26eef1af5586397715e4
Manu Joan Saharrea <saharreamanu7@gmail.com>
2025-05-08 16:31:02 -0300

    Trabajos Practicos Realizados

---
+++ EJ2/data.s
@@ -0,0 +1,25 @@
+/*
+    En este archivo se definen las constantes que se usan en el programa
+    y se reservan las zonas de memoria necesarias para el programa.
+ */
+
+.ifndef data_s
+.equ data_s, 0
+
+
+.data
+
+//Colores
+
+sand_color: .word    0xFCF89A  
+
+/* Cosas de la pantalla */
+.equ SCREEN_WIDTH, 640
+.equ SCREEN_HEIGH, 480
+.equ BITS_PER_PIXEL, 32
+
+.equ GPIO_BASE, 0x3f200000
+.equ GPIO_GPFSEL0, 0x00
+.equ GPIO_GPLEV0, 0x34
+
+.endif
